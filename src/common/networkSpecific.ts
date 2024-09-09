2d6d4eb53531f94514f2e94dcecb5cfe91df8f13
spennyp <hello@spencerperkins.me>
2024-09-08 21:45:09 -0700

    Added missing network specific config, fixes Optimism, Scroll, and testnets

--- src/common/networkSpecific.ts
+++ src/common/networkSpecific.ts
@@ -6,6 +6,12 @@ namespace SupportedChain {
     export const POLYGON = "matic";
     export const BASE = "base";
     export const ARBITRUM = "arbitrum-one";
+    export const OPTIMISM = "optimism";
+    export const SCROLL = "scroll";
+
+    // Testnet
+    export const SEPOLIA = "sepolia";
+    export const BASE_SEPOLIA = "base-sepolia";
 }
 
 const configuratorProxyAddress = new Map<string, Address>()
@@ -13,6 +19,10 @@ const configuratorProxyAddress = new Map<string, Address>()
     .set(SupportedChain.POLYGON, Address.fromString("0x83E0F742cAcBE66349E3701B171eE2487a26e738"))
     .set(SupportedChain.BASE, Address.fromString("0x45939657d1CA34A8FA39A924B71D28Fe8431e581"))
     .set(SupportedChain.ARBITRUM, Address.fromString("0xb21b06D71c75973babdE35b49fFDAc3F82Ad3775"))
+    .set(SupportedChain.OPTIMISM, Address.fromString("0x84E93EC6170ED630f5ebD89A1AAE72d4F63f2713"))
+    .set(SupportedChain.SCROLL, Address.fromString("0xECAB0bEEa3e5DEa0c35d3E69468EAC20098032D7"))
+    .set(SupportedChain.SEPOLIA, Address.fromString("0xc28aD44975C614EaBe0Ed090207314549e1c6624"))
+    .set(SupportedChain.BASE_SEPOLIA, Address.fromString("0x090a2b1fc84d0b5141d5D5608b12Db19201aE5a6"))
     .set("fallback", ZERO_ADDRESS);
 
 const cometRewardsAddress = new Map<string, Address>()
@@ -20,6 +30,10 @@ const cometRewardsAddress = new Map<string, Address>()
     .set(SupportedChain.POLYGON, Address.fromString("0x45939657d1CA34A8FA39A924B71D28Fe8431e581"))
     .set(SupportedChain.BASE, Address.fromString("0x123964802e6ABabBE1Bc9547D72Ef1B69B00A6b1"))
     .set(SupportedChain.ARBITRUM, Address.fromString("0x88730d254A2f7e6AC8388c3198aFd694bA9f7fae"))
+    .set(SupportedChain.OPTIMISM, Address.fromString("0x443EA0340cb75a160F31A440722dec7b5bc3C2E9"))
+    .set(SupportedChain.SCROLL, Address.fromString("0x70167D30964cbFDc315ECAe02441Af747bE0c5Ee"))
+    .set(SupportedChain.SEPOLIA, Address.fromString("0x8bF5b658bdF0388E8b482ED51B14aef58f90abfD"))
+    .set(SupportedChain.BASE_SEPOLIA, Address.fromString("0x3394fa1baCC0b47dd0fF28C8573a476a161aF7BC"))
     .set("fallback", ZERO_ADDRESS);
 
 // Only needed because ETH markets are priced in ETH, not USDC
@@ -27,7 +41,11 @@ const wethMarketProxyAddress = new Map<string, Address>()
     .set(SupportedChain.MAINNET, Address.fromString("0xA17581A9E3356d9A858b789D68B4d866e593aE94"))
     .set(SupportedChain.POLYGON, ZERO_ADDRESS) // None exists
     .set(SupportedChain.BASE, Address.fromString("0x46e6b214b524310239732D51387075E0e70970bf"))
-    .set(SupportedChain.ARBITRUM, ZERO_ADDRESS) // None exists
+    .set(SupportedChain.ARBITRUM, Address.fromString("0x6f7D514bbD4aFf3BcD1140B7344b32f063dEe486")) 
+    .set(SupportedChain.OPTIMISM, Address.fromString("0xE36A30D249f7761327fd973001A32010b521b6Fd"))
+    .set(SupportedChain.SCROLL, ZERO_ADDRESS) // None exists
+    .set(SupportedChain.SEPOLIA, Address.fromString("0x2943ac1216979aD8dB76D9147F64E61adc126e96"))
+    .set(SupportedChain.BASE_SEPOLIA, Address.fromString("0x61490650AbaA31393464C3f34E8B29cd1C44118E"))
     .set("fallback", ZERO_ADDRESS);
 
 const compTokenAddress = new Map<string, Address>()
@@ -35,6 +53,10 @@ const compTokenAddress = new Map<string, Address>()
     .set(SupportedChain.POLYGON, Address.fromString("0x8505b9d2254A7Ae468c0E9dd10Ccea3A837aef5c"))
     .set(SupportedChain.BASE, Address.fromString("0x9e1028f5f1d5ede59748ffcee5532509976840e0"))
     .set(SupportedChain.ARBITRUM, Address.fromString("0x354A6dA3fcde098F8389cad84b0182725c6C91dE"))
+    .set(SupportedChain.OPTIMISM, Address.fromString("0x7e7d4467112689329f7E06571eD0E8CbAd4910eE"))
+    .set(SupportedChain.SCROLL, Address.fromString("0x643e160a3C3E2B7eae198f0beB1BfD2441450e86"))
+    .set(SupportedChain.SEPOLIA, Address.fromString("0xA6c8D1c55951e8AC44a0EaA959Be5Fd21cc07531"))
+    .set(SupportedChain.BASE_SEPOLIA, Address.fromString("0x2f535da74048c0874400f0371Fba20DF983A56e2"))
     .set("fallback", ZERO_ADDRESS);
 
 const chainlinkEthUsdPriceFeedAddress = new Map<string, Address>()
@@ -42,6 +64,10 @@ const chainlinkEthUsdPriceFeedAddress = new Map<string, Address>()
     .set(SupportedChain.POLYGON, Address.fromString("0xF9680D99D6C9589e2a93a78A04A279e509205945"))
     .set(SupportedChain.BASE, Address.fromString("0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70"))
     .set(SupportedChain.ARBITRUM, Address.fromString("0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612"))
+    .set(SupportedChain.OPTIMISM, Address.fromString("0x13e3Ee699D1909E989722E753853AE30b17e08c5"))
+    .set(SupportedChain.SCROLL, Address.fromString("0x6bF14CB0A831078629D993FDeBcB182b21A8774C"))
+    .set(SupportedChain.SEPOLIA, Address.fromString("0x694AA1769357215DE4FAC081bf1f309aDC325306"))
+    .set(SupportedChain.BASE_SEPOLIA, Address.fromString("0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1"))
     .set("fallback", ZERO_ADDRESS);
 
 const chainlinkCompUsdPriceFeedAddress = new Map<string, Address>()
@@ -49,6 +75,10 @@ const chainlinkCompUsdPriceFeedAddress = new Map<string, Address>()
     .set(SupportedChain.POLYGON, Address.fromString("0x2A8758b7257102461BC958279054e372C2b1bDE6"))
     .set(SupportedChain.BASE, Address.fromString("0x9DDa783DE64A9d1A60c49ca761EbE528C35BA428"))
     .set(SupportedChain.ARBITRUM, Address.fromString("0xe7C53FFd03Eb6ceF7d208bC4C13446c76d1E5884"))
+    .set(SupportedChain.OPTIMISM, Address.fromString("0xe1011160d78a80E2eEBD60C228EEf7af4Dfcd4d7"))
+    .set(SupportedChain.SCROLL, Address.fromString("0x6726C678feE07B25BBE67bC720728652E4129369"))
+    .set(SupportedChain.SEPOLIA, ZERO_ADDRESS) // Price feed doesn't exist
+    .set(SupportedChain.BASE_SEPOLIA, ZERO_ADDRESS) // Price feed doesn't exist
     .set("fallback", ZERO_ADDRESS);
 
 /**
